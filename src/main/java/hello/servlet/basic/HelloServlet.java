cdfd4aa6f81a39aa5a8a602db3f9c54f595fe9f2
Pyunji <vusguswl@gmail.com>
2021-06-14 15:49:56 +0900

    Create: Hello 서블릿

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,28 @@
+package hello.servlet.basic;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override   // Ctrl + O
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        // 헤더
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        // 바디
+        response.getWriter().write("hello " + username);
+    }
+}
